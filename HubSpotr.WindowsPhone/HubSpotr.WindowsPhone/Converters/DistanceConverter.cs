097cff0542c5fbbf639b4eea0a9d4f8bbb7b31a8
Samir Hafez <rhymecheat@hotmail.com>
2014-05-18 16:54:53 +0100

    - Forgot some files in the last commit.

---
+++ HubSpotr.WindowsPhone/HubSpotr.WindowsPhone/Converters/DistanceConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace HubSpotr.WindowsPhone.Converters
+{
+    public class DistanceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var distance = (double)value;
+
+            var weight = "m";
+
+            if (distance > 1000)
+            {
+                distance /= 1000;
+                weight = "km";
+            }
+
+            return string.Format("{0} {1}", (int)distance, weight);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
